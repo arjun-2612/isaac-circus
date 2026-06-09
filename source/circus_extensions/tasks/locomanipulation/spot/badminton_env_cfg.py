056c121bc280ced2aa829709edc8e9eb50e4ec1b
arjuns-code-center <arjun.viswanathan2612@gmail.com>
2026-06-09 11:51:51 -0400

    reduce approach reward weight and increase velocity reward weight. Need to see if robot can stay and swing

--- source/circus_extensions/tasks/locomanipulation/spot/badminton_env_cfg.py
+++ source/circus_extensions/tasks/locomanipulation/spot/badminton_env_cfg.py
@@ -326,7 +326,7 @@ class SpotBadmintonRewardsCfg:
     # -- task: approach and orientation
     ee_approach = RewardTermCfg(
         func=spot_mdp.ee_approach_reward,
-        weight=5.0,
+        weight=2.0,
         params={
             "ee_frame_cfg": SceneEntityCfg("spot_badminton_ee"),
             "std": 0.5, 
@@ -343,15 +343,15 @@ class SpotBadmintonRewardsCfg:
     )
 
     # -- task: swing and follow through
-    # ee_velocity = RewardTermCfg(
-    #     func=spot_mdp.ee_velocity_tracking_reward,
-    #     weight=1.0,
-    #     params={
-    #         "asset_cfg": SceneEntityCfg("robot", body_names="arm0_racket_face"),
-    #         "swing_speed": 12.0, 
-    #         "half_width": 0.35,
-    #     },
-    # )
+    ee_velocity = RewardTermCfg(
+        func=spot_mdp.ee_velocity_tracking_reward,
+        weight=10.0,
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="arm0_racket_face"),
+            "swing_speed": 12.0, 
+            "half_width": 0.25,
+        },
+    )
     # ee_follow_through = RewardTermCfg(
     #     func=spot_mdp.ee_follow_through_reward,
     #     weight=1.0,
